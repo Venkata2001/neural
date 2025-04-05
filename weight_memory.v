1d24e4d5b5ccb73766616a039bc5c44fdac5d0c6
Venkata2001 <105587239+Venkata2001@users.noreply.github.com>
2025-04-05 18:04:24 +0530

    Add files via upload

--- weight_memory.v
+++ weight_memory.v
@@ -18,26 +18,26 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+`define READ_RAM
 
-
-module weight_memory #(parameter DATA_WIDTH = 31, NO_OF_WEIGHT = 784, ADDRS_WIDTH = $clog2(NO_OF_WEIGHT))(
+module weight_memory #(parameter DATA_WIDTH = 32, NO_OF_WEIGHT = 784, ADDRS_WIDTH = $clog2(NO_OF_WEIGHT))(
     input clk,reset_n,
-    input s_axis_tvalid,
-    input wr_en,
-    input rd_en,
-    input rd_ram_or_rom,
-    input [DATA_WIDTH-1:0]dina,
-    input [ADDRS_WIDTH-1:0]addra_in,
+    input s_axis_tvalid,re_en,
+    input [DATA_WIDTH-1:0]s_axis_tdata,
     output reg m_axis_tvalid,
-    output wire [DATA_WIDTH-1:0]m_axis_tdata
+    output reg [DATA_WIDTH-1:0]m_axis_tdata
     );
     
     reg [ADDRS_WIDTH-1:0]addr = 0;
-    reg [ADDRS_WIDTH-1:0]addrb_1 = 0;
     reg ena   = 0;
     reg enb_1 = 0;
     wire [DATA_WIDTH-1:0]data_o;
     wire [DATA_WIDTH-1:0]data_b;
+    
+    reg ena_1 =0, wea = 0;
+    reg [ADDRS_WIDTH-1:0]addra_in;
+    reg [ADDRS_WIDTH-1:0]addrb;
+    reg [DATA_WIDTH-1:0]dina;
     
     blk_mem_gen_0 weight_brom (
         .clka(clk),    // input wire clka
@@ -48,8 +48,8 @@ module weight_memory #(parameter DATA_WIDTH = 31, NO_OF_WEIGHT = 784, ADDRS_WIDT
         
     blk_mem_gen_1  weight_bram(
         .clka(clk),    // input wire clka
-        .ena(wr_en),      // input wire ena
-        .wea(wr_en),      // input wire [0 : 0] wea
+        .ena(ena_1),      // input wire ena
+        .wea(wea),      // input wire [0 : 0] wea
         .addra(addra_in),  // input wire [9 : 0] addra
         .dina(dina),    // input wire [31 : 0] dina
         .clkb(clk),    // input wire clkb
@@ -57,58 +57,102 @@ module weight_memory #(parameter DATA_WIDTH = 31, NO_OF_WEIGHT = 784, ADDRS_WIDT
         .addrb(addrb),  // input wire [9 : 0] addrb
         .doutb(data_b)  // output wire [31 : 0] doutb
         );
-    integer sample_count, sample_count_1 =0;
+    integer sample_count=0, sample_count_1 =0,rd_count=0;
     
-    always@(posedge(clk))
-    begin
-        if(!reset_n)
+    `ifdef READ_RAM
+        always@(posedge(clk))
         begin
-            m_axis_tvalid <= 0;
-            addr          <= 0;
-            ena           <= 0; 
-            enb_1         <= 0;           
-            sample_count  <= 0; 
-            sample_count_1<= 0; 
-            addrb_1       <= {ADDRS_WIDTH{1'b1}};           
-        end
-        else begin
-            if(rd_ram_or_rom == 1)begin
-                if(s_axis_tvalid== 1)begin
-                    ena       = 1;
-                    addr      = addr +1;
+            if(!reset_n)
+            begin
+                m_axis_tvalid <= 0;
+                addr          <= 0;
+                ena           <= 0;          
+                sample_count  <= 0; 
+                sample_count_1<= 0; 
+                m_axis_tdata  <= 0;          
+            end
+            else begin
+                if(sample_count < NO_OF_WEIGHT + 2)begin
+                    ena       <= 1;
+                    addr      <= addr +1;
+                    sample_count<= sample_count + 1;
+                    if(sample_count >= 2)begin
+                        m_axis_tvalid  <= 1;
+                        m_axis_tdata   <= dina;
+                    end
+                    else begin
+                        m_axis_tvalid <= 0;
+                    end
                 end
                 else begin
                     ena    <= 0;
+                    addr   <= 0;
+                    m_axis_tvalid <= 0;
                 end
-                if(sample_count <  NO_OF_WEIGHT-1 && addr > 2)begin
-                    m_axis_tvalid  <= 1;
-                    sample_count = sample_count + 1;               
-                end
-                else begin
-                    m_axis_tvalid  <= 0;
-                end
-            end
-            else begin
-               //read data from bram
-               if(rd_en  == 1 && rd_ram_or_rom == 0)begin
-                 if(s_axis_tvalid == 1)begin
-                    enb_1  <= 1;
-                    addrb_1<= addrb_1 + 1;
-                 end
-                 else begin
-                     enb_1    <= 0;
-                 end
-                 if(sample_count_1 <  NO_OF_WEIGHT-1 && addrb_1 > 2)begin
-                     m_axis_tvalid  <= 1;
-                     sample_count_1 = sample_count_1 + 1;               
-                 end
-                 else begin
-                     m_axis_tvalid  <= 0;
-                 end
-               end
             end
         end
-    end
- 
- assign m_axis_tdata   =  (rd_en)? data_b : data_o;  
+    `else
+        always@(posedge(clk))
+        begin
+            if(!reset_n)begin
+                m_axis_tvalid  <= 0;
+                m_axis_tdata   <= 0;
+                sample_count_1 <= 0;
+                addra_in       <= 0;
+                ena_1    <= 0;
+                wea      <= 0;  
+                addrb    <= 0;
+                rd_count <= 0;
+                enb_1    <= 0;
+            end
+            else begin
+                if(s_axis_tvalid == 1)begin
+                    if(sample_count_1 < NO_OF_WEIGHT)begin
+                        ena_1  <= 1;
+                        wea    <= 1;
+                        addra_in <= sample_count_1;
+                        dina   <= s_axis_tdata;
+                    end 
+                    else begin
+                        sample_count_1 <= 0;
+                        addra_in       <= 0;
+                        ena_1    <= 0;
+                        wea      <= 0;  
+                    end                    
+                end
+                else begin
+                   sample_count_1 <= 0;
+                   addra_in       <= 0;
+                   ena_1    <= 0;
+                   wea      <= 0;   
+                   if(re_en == 1)begin
+                       if(rd_count < NO_OF_WEIGHT + 2)begin
+                           enb_1  <= 1;
+                           addrb  <= addrb + 1;
+                           rd_count<= rd_count + 1;
+                           if(rd_count >= 2)begin
+                               m_axis_tdata <= data_b;
+                               m_axis_tvalid<= 1;
+                           end
+                           else begin
+                               m_axis_tvalid <= 0;
+                               enb_1   <= 0;
+                               addrb   <= 0;
+                           end
+                       end
+                       else begin
+                           m_axis_tvalid <= 0;
+                           enb_1   <= 0;
+                           addrb   <= 0;
+                       end
+                   end
+                   else begin
+                       m_axis_tvalid <= 0; 
+                       enb_1   <= 0;       
+                       addrb   <= 0;                   
+                   end
+                end
+            end
+        end        
+    `endif 
 endmodule
